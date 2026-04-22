bb3407932d6d254fa7921398d8ac700214eae29a
NEKOmatta <nekomatta77@gmail.com>
2026-04-23 01:00:30 +0800

    слоты

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,9 +1,9 @@
 // src/config/api.ts
 
-// Теперь Vercel сам перехватывает /api и шлет куда надо
+// Если это Vercel, используем безопасный мост /backend-api
 export const AGGREGATOR_API_URL = import.meta.env.DEV 
   ? 'http://193.124.66.221:22777/api' 
-  : '/api'; 
+  : '/backend-api';
 
 export const ENDPOINTS = {
   GAMES_LIST: '/public-games',
