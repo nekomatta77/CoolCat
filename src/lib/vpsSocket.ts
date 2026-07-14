fb650af55acd50f102cb1f1de76636005e106738
NEKOmatta <nekomatta77@gmail.com>
2026-07-15 06:39:09 +0800

    впс

--- src/lib/vpsSocket.ts
+++ src/lib/vpsSocket.ts
@@ -1,8 +1,10 @@
-// src/lib/vpsSocket.ts
 import { io } from 'socket.io-client';
 
-// НОВЫЙ ЗАЩИЩЕННЫЙ АДРЕС ТВОЕГО СЕРВЕРА
-const VPS_URL = 'https://coolcat-api.duckdns.org';
+// Раскомментируй эту строку для работы на компьютере:
+const VPS_URL = 'http://localhost:3001'; 
+
+// А эту строку закомментируй (поставь // в начале), пока тестируешь локально:
+// const VPS_URL = 'https://coolcat-api.duckdns.org';
 
 export const vpsSocket = io(VPS_URL, {
   transports: ['websocket', 'polling'],
